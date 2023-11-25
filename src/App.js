c03ebed17898e4f51341b88f7a184ee62bc5f5d6
Anatolii <anatolii.boboc@mail.ru>
2023-11-25 14:21:46 +0100

    advice project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [advice, setAdvice] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchAdvice = () => {
+    setLoading(true);
+    axios.get('https://api.adviceslip.com/advice')
+      .then(res => {
+        setAdvice(res.data.slip.advice);
+      })
+      .catch(err => {
+        setError(err); 
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="card">
+        {error && <div>Error: {error.message}</div>}
+        <h1>{advice}</h1>
+        <button onClick={fetchAdvice} disabled={loading}>
+          {loading ? 'Loading...' : 'Get an advice'}
+        </button>
+      </div>
     </div>
   );
 }
